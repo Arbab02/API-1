ad209b93de81e44068248c1d35f164415671f60d
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-01-18 15:30:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('sharedMedia', (media) => {
+    // Broadcast the shared media to all other sockets
+    socket.broadcast.emit('sharedMedia', media);
+  });
+
+  socket.on('disconnect', () => {
+    // Handle disconnect event
+  });
+
+  socket.on('reconnect', () => {
+    // Handle reconnect event
+  });
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
