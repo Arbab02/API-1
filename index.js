cb87e63cf01da62df91ffcd23e954201fc18921c
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-01-18 04:39:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
