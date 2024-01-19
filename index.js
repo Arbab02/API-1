c922f7852fefb87c4e5db8b5ad761404cb4fd672
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-01-18 22:50:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-require('dotenv').config()
+// server.js
+
+require('dotenv').config();
 
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(cors());
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  socket.on('sharedMedia', (media) => {
-    // Broadcast the shared media to all other sockets
-    socket.broadcast.emit('sharedMedia', media);
+const messageSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+io.on('connection', async (socket) => {
+  try {
+    // Retrieve messages from the database and send them to the connected client
+    const messages = await Message.find({}).exec();
+    socket.emit('chat history', messages);
+  } catch (err) {
+    console.error(err);
+  }
+
+  socket.on('chat message', async (msg) => {
+    try {
+      const newMessage = new Message({ user: msg.user, text: msg.text });
+      await newMessage.save();
+
+      // Broadcast the message to all connected clients
+      io.emit('chat message', { user: msg.user, text: msg.text });
+    } catch (err) {
+      console.error(err);
+    }
   });
 
   socket.on('disconnect', () => {
